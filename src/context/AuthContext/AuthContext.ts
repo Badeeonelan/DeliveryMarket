2e2c16abae96a700e94cc58142fee7d9e2a60f2c
Badeeonelan <gaa3538@gmail.com>
2025-02-11 12:05:15 +0300

    Revert "add AuthContext & AuthContextProvider component"
    This reverts commit e0beff425b990d480ab18a77d1636a71a2b14918.

--- src/context/AuthContext/AuthContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-type AuthContextType = {
-	isAuth: boolean,
-	setIsAuth: (value: boolean) => void;
-}
-
-export const AuthContext = createContext<AuthContextType | null>(null);
