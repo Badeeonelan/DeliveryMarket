258fa9bbe5ea0f6d0e70eebab45b89cd2858e3e9
Badeeonelan <gaa3538@gmail.com>
2025-02-25 09:41:01 +0300

    add Redux+LocalStorage auth

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./user.slice";
+import userSlice, { JWT_STORAGE_KEY } from "./user.slice";
 import { useSelector } from "react-redux";
+import { saveState } from "./storage";
 
 export const store = configureStore({
 	reducer: {
 		user: userSlice
 	}
 });
+
+store.subscribe(() => {
+	saveState(JWT_STORAGE_KEY, store.getState().user.jwt);
+})
 
 export type TRootState = ReturnType<typeof store.getState>;
 export type TAppDispatch = typeof store.dispatch;
